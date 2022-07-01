0628709c251573ebf1d64ac9ced4010bb356b1db
Shailendra Chaudhary <sp1609729@gmail.com>
2022-07-01 20:54:44 +0530

    Add files via upload

---
+++ A_A_B_Trial_Problem_.cpp
@@ -0,0 +1,19 @@
+typedef long long ll;
+#include<bits/stdc++.h>
+#define mod 1000000007
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--){
+      int a,b;
+      cin>>a>>b;
+
+      cout<<(a+b)<<endl;
+
+     }
+return 0;
+}
